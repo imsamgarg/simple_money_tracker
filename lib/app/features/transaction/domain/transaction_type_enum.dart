d8f011d55523b030ec68a0495392148a49ff4a7a
sam garg <smlgarg123@gmail.com>
2022-07-31 22:26:31 +0530

    fromString factory added to transactiontype enum

--- lib/app/features/transaction/domain/transaction_type_enum.dart
+++ lib/app/features/transaction/domain/transaction_type_enum.dart
@@ -4,4 +4,10 @@ enum TransactionType {
 
   const TransactionType(this.value);
   final String value;
+
+  factory TransactionType.fromString(String type) {
+    if (type == TransactionType.expense.value) return TransactionType.expense;
+    if (type == TransactionType.income.value) return TransactionType.income;
+    throw UnsupportedError("$type not supported");
+  }
 }
