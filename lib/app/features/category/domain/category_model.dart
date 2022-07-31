3e7345808a1495db16aa809db4a4ad5792fc75fa
sam garg <smlgarg123@gmail.com>
2022-07-31 22:29:25 +0530

    category model updated

--- lib/app/features/category/domain/category_model.dart
+++ lib/app/features/category/domain/category_model.dart
@@ -1,48 +1,73 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 
+import 'package:simple_money_tracker/app/features/transaction/domain/transaction_type_enum.dart';
+
 class CategoryModel {
+  final int? id;
   final String categoryName;
   final String? imagePath;
+  final TransactionType type;
   CategoryModel({
+    this.id,
     required this.categoryName,
     this.imagePath,
+    required this.type,
   });
 
   CategoryModel copyWith({
+    int? id,
     String? categoryName,
     String? imagePath,
+    TransactionType? type,
   }) {
     return CategoryModel(
+      id: id ?? this.id,
       categoryName: categoryName ?? this.categoryName,
       imagePath: imagePath ?? this.imagePath,
+      type: type ?? this.type,
     );
   }
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
+      if (id != null) 'id': id,
       'categoryName': categoryName,
-      'imagePath': imagePath,
+      if (imagePath != null) 'imagePath': imagePath,
+      'type': type.value,
     };
   }
 
   factory CategoryModel.fromMap(Map<String, dynamic> map) {
     return CategoryModel(
+      id: map['id'] as int,
       categoryName: map['categoryName'] as String,
-      imagePath: map['imagePath'] as String?,
+      imagePath: map['imagePath'] != null ? map['imagePath'] as String : null,
+      type: TransactionType.fromString(
+        map['type'] as String,
+      ),
     );
   }
 
   @override
-  String toString() =>
-      'CategoryModel(categoryName: $categoryName, imagePath: $imagePath)';
+  String toString() {
+    return 'CategoryModel(id: $id, categoryName: $categoryName, imagePath: $imagePath, transactionType: $type)';
+  }
 
   @override
   bool operator ==(covariant CategoryModel other) {
     if (identical(this, other)) return true;
 
-    return other.categoryName == categoryName && other.imagePath == imagePath;
+    return other.id == id &&
+        other.categoryName == categoryName &&
+        other.imagePath == imagePath &&
+        other.type == type;
   }
 
   @override
-  int get hashCode => categoryName.hashCode ^ imagePath.hashCode;
+  int get hashCode {
+    return id.hashCode ^
+        categoryName.hashCode ^
+        imagePath.hashCode ^
+        type.hashCode;
+  }
 }
