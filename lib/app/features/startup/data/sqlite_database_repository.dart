397d7489cb9727c883a43017a9de19312b968ea2
sam garg <smlgarg123@gmail.com>
2022-08-10 20:49:21 +0530

    batch method added

--- lib/app/features/startup/data/sqlite_database_repository.dart
+++ lib/app/features/startup/data/sqlite_database_repository.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:async';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -38,6 +37,8 @@ class SqliteDatabaseRepository {
     final query = queriesOnDatabaseCreation.join(";");
     return db.execute(query);
   }
+
+  Batch batch() => db.batch();
 }
 
 final sqliteDatabaseProvider = Provider<SqliteDatabaseRepository>((ref) {
