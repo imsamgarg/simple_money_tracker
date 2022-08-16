545a7d94ecce7547b61f0f35d73b095721bfa703
sam garg <smlgarg123@gmail.com>
2022-08-16 22:47:14 +0530

    bug fixes

--- lib/app/features/transaction/presentation/controllers/add_expense_controller.dart
+++ lib/app/features/transaction/presentation/controllers/add_expense_controller.dart
@@ -1,42 +1,37 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:simple_money_tracker/app/core/utils/unique_id_generator.dart';
 import 'package:simple_money_tracker/app/features/category/domain/category_model.dart';
 import 'package:simple_money_tracker/app/features/transaction/application/transaction_service.dart';
 import 'package:simple_money_tracker/app/features/transaction/domain/transaction_model.dart';
 import 'package:simple_money_tracker/app/features/transaction/domain/transaction_type_enum.dart';
+import 'package:simple_money_tracker/app/features/transaction/presentation/controllers/transaction_validators.dart';
 
-class AddExpenseController extends StateNotifier<AsyncValue<void>> {
-  final TransactionService service;
+class AddExpenseController extends StateNotifier<AsyncValue<void>>
+    with TransactionValidator {
+  final TransactionService _service;
 
-  AddExpenseController(this.service) : super(const AsyncData(null));
+  AddExpenseController(this._service) : super(const AsyncData(null));
 
   Future<void> addExpense({
     required double amount,
     required CategoryModel category,
+    required DateTime time,
+    String? description,
+    String? notes,
   }) async {
     state = const AsyncLoading();
-    final txnModel = createTxnModel(amount: amount, category: category);
-    state = await AsyncValue.guard(
-      () => service.addTransaction(txnModel),
-    );
-  }
-
-  @visibleForTesting
-  TransactionModel createTxnModel({
-    required double amount,
-    required CategoryModel category,
-    //For testing
-    DateTime? time,
-  }) {
-    final txnTime = time ?? DateTime.now();
-    final id = getUniqueId(time: txnTime);
-    return TransactionModel(
-      transactionType: TransactionType.expense,
+    final id = getUniqueId();
+    final txnModel = TransactionModel(
       amount: amount,
-      time: txnTime,
-      id: id,
+      time: time,
       category: category,
+      description: description,
+      notes: notes,
+      id: id,
+      transactionType: TransactionType.expense,
+    );
+    state = await AsyncValue.guard(
+      () => _service.addTransaction(txnModel),
     );
   }
 }
