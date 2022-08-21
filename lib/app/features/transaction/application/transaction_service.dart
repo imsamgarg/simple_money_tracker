763f6f56a61e0bb5fc50fdf35fe8ab1bdb30b6c7
sam garg <smlgarg123@gmail.com>
2022-08-21 23:51:42 +0530

    transaction service transactions stream added

--- lib/app/features/transaction/application/transaction_service.dart
+++ lib/app/features/transaction/application/transaction_service.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:simple_money_tracker/app/features/startup/data/sqlite_database_repository.dart';
 import 'package:simple_money_tracker/app/features/summary/data/sqlite_summary_batch_op_repository.dart';
@@ -10,23 +12,49 @@ class TransactionService {
   final LocalTransactionRepository txnRepo;
   final SqliteDatabaseRepository dbRepo;
 
-  TransactionService({required this.dbRepo, required this.txnRepo});
+  final StreamController<List<TransactionModel>> _expensesStreamController;
+  final StreamController<List<TransactionModel>> _incomeStreamController;
+
+  TransactionService({required this.dbRepo, required this.txnRepo})
+      : _expensesStreamController = StreamController.broadcast(),
+        _incomeStreamController = StreamController.broadcast();
+
+  Stream<List<TransactionModel>> get watchExpenses =>
+      _expensesStreamController.stream;
+  Stream<List<TransactionModel>> get watchIncomes =>
+      _incomeStreamController.stream;
 
   /// Add Transations
-  Future<void> addTransaction(TransactionModel txnModel) {
+  Future<void> addTransaction(TransactionModel txnModel) async {
     final batch = dbRepo.batch();
     final transactionBatchRepo = SqliteTransactionBatchOpRepository(batch);
     final summartBatchRepo = SqliteSummaryBatchRepository(batch);
 
     transactionBatchRepo.addTransaction(txnModel);
     final amount = txnModel.amount;
-    if (txnModel.transactionType == TransactionType.expense) {
+    final isExpense = txnModel.transactionType == TransactionType.expense;
+
+    if (isExpense) {
       summartBatchRepo.addExpenses(amount);
     } else {
       summartBatchRepo.addIncome(amount);
     }
 
-    return batch.commit();
+    await batch.commit();
+
+    if (isExpense) {
+      if (!_expensesStreamController.hasListener) return;
+
+      return _expensesStreamController.add(
+        (await watchExpenses.last).toList()..add(txnModel),
+      );
+    }
+
+    if (!_incomeStreamController.hasListener) return;
+
+    return _incomeStreamController.add(
+      (await watchIncomes.last).toList()..add(txnModel),
+    );
   }
 
   /// Delete Transations
@@ -62,4 +90,22 @@ final transactionServiceProvider = Provider<TransactionService>((ref) {
   final dbRepo = ref.read(sqliteDatabaseProvider);
 
   return TransactionService(txnRepo: localRepo, dbRepo: dbRepo);
+});
+
+final incomeTransactionsProvider =
+    StreamProvider<List<TransactionModel>>((ref) {
+  final service = ref.watch(transactionServiceProvider);
+  service
+      .getAllIncomes()
+      .then((txns) => service._incomeStreamController.add(txns));
+  return service.watchIncomes;
+});
+
+final expenseTransactionsProvider =
+    StreamProvider<List<TransactionModel>>((ref) {
+  final service = ref.watch(transactionServiceProvider);
+  service
+      .getAllExpenses()
+      .then((txns) => service._expensesStreamController.add(txns));
+  return service.watchExpenses;
 });
