5052772ae8a8f4e222b130100bd1ddf53d2ddb67
sam garg <smlgarg123@gmail.com>
2022-08-27 13:24:53 +0530

    defaultRadius added

--- lib/app/core/constants/sizes.dart
+++ lib/app/core/constants/sizes.dart
@@ -2,7 +2,8 @@ import 'package:custom_utils/custom_utils.dart';
 
 const kTextFieldDefaultRadius = radius8;
 const kTextFieldDefaultBorderRadius = borderRadius8;
-const kButtonDefaultRadius = radius8;
+const kButtonDefaultRadius = kDefaultRadius;
+const kDefaultRadius = radius16;
 const rc24 = radius24;
 const rc16 = radius16;
 const r24 = dp24;
