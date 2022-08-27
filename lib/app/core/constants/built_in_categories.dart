47185089ec097e6e1a8fa5a0a65d19e3f164d2f6
sam garg <smlgarg123@gmail.com>
2022-08-27 13:24:21 +0530

    added some built-in-categories

--- lib/app/core/constants/built_in_categories.dart
+++ lib/app/core/constants/built_in_categories.dart
@@ -7,4 +7,29 @@ final builtInCategories = <CategoryModel>[
     categoryName: "Food",
     type: TransactionType.expense,
   ),
+  const CategoryModel(
+    id: 2,
+    categoryName: "Rent",
+    type: TransactionType.expense,
+  ),
+  const CategoryModel(
+    id: 3,
+    categoryName: "Travel",
+    type: TransactionType.expense,
+  ),
+  const CategoryModel(
+    id: 4,
+    categoryName: "Shopping",
+    type: TransactionType.expense,
+  ),
+  const CategoryModel(
+    id: 5,
+    categoryName: "Salary",
+    type: TransactionType.income,
+  ),
+  const CategoryModel(
+    id: 6,
+    categoryName: "Gift",
+    type: TransactionType.income,
+  ),
 ];
