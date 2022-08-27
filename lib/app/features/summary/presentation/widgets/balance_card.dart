05c51d66134d88531db7c24032cc178de74380fe
sam garg <smlgarg123@gmail.com>
2022-08-27 22:57:20 +0530

    card polished

--- lib/app/features/summary/presentation/widgets/balance_card.dart
+++ lib/app/features/summary/presentation/widgets/balance_card.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:custom_utils/custom_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -48,33 +49,116 @@ class _BalanceCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return Container(
-      margin: p16,
-      padding: p16,
-      decoration: BoxDecoration(
-        color: theme.primaryColor,
-        borderRadius: const BorderRadius.all(kDefaultRadius),
-      ),
-      child: Column(
-        children: [
-          const Text("Total Balance"),
-          Text(
-            totalBalance,
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: theme.textTheme.headlineSmall!.fontSize,
+
+    return Builder(
+      builder: (context) {
+        return DefaultTextStyle(
+          style: const TextStyle(color: Colors.white),
+          child: Container(
+            margin: p16,
+            padding: p16,
+            decoration: BoxDecoration(
+              color: theme.primaryColor,
+              // gradient: LinearGradient(
+              //   colors: [
+              //     theme.primaryColor,
+              //   ],
+              // ),
+              borderRadius: const BorderRadius.all(kDefaultRadius),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                const Text(
+                  "Total Balance",
+                  style: TextStyle(
+                    fontSize: 13,
+                  ),
+                ),
+                Text(
+                  totalBalance,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: theme.textTheme.headlineSmall!.fontSize,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    _Subsection(
+                      amount: totalIncome,
+                      icon: const Icon(
+                        Icons.arrow_upward_rounded,
+                        color: Colors.green,
+                      ),
+                      title: "Income",
+                    ),
+                    _Subsection(
+                      amount: totalExpenses,
+                      icon: const Icon(
+                        Icons.arrow_downward_rounded,
+                        color: Colors.red,
+                      ),
+                      title: "Expenses",
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(totalIncome),
-              Text(totalExpenses),
-            ],
+        );
+      },
+    );
+  }
+}
+
+class _Subsection extends StatelessWidget {
+  const _Subsection({
+    super.key,
+    required this.title,
+    required this.icon,
+    required this.amount,
+  });
+
+  final String title;
+  final Icon icon;
+  final String amount;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        DecoratedBox(
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            shape: BoxShape.circle,
           ),
-        ],
-      ),
+          child: Padding4(
+            child: icon,
+          ),
+        ),
+        horSpacing12,
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+            Text(
+              amount,
+              style: const TextStyle(
+                fontSize: 16,
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
