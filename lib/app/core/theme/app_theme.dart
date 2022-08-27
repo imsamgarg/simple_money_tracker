24510b4ea66b75f0cb201633d825fdfea43729f1
sam garg <smlgarg123@gmail.com>
2022-08-27 22:15:55 +0530

    light theme added

---
+++ lib/app/core/theme/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:simple_money_tracker/app/core/theme/text_theme.dart';
+
+final lightTheme = ThemeData(
+  textTheme: defaultTextTheme,
+  // backgroundColor: AppColors.bgColor,
+  // colorSchemeSeed: AppColors.primaryColor,
+);
