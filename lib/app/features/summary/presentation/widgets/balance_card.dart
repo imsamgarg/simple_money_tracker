5c6657d6dae5042e9a72d469dc0d9fa0d25a7b0f
sam garg <smlgarg123@gmail.com>
2022-08-29 23:35:49 +0530

    balance card updated to be responsive

--- lib/app/features/summary/presentation/widgets/balance_card.dart
+++ lib/app/features/summary/presentation/widgets/balance_card.dart
@@ -1,12 +1,14 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:custom_utils/custom_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:simple_money_tracker/app/core/constants/sizes.dart';
+import 'package:simple_money_tracker/app/core/widgets/responsive_widget.dart';
 import 'package:simple_money_tracker/app/features/summary/application/summary_service.dart';
 
 class BalanceCard extends ConsumerWidget {
-  const BalanceCard({super.key});
+  const BalanceCard({this.padding = p16, super.key});
+
+  final EdgeInsets padding;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -14,18 +16,21 @@ class BalanceCard extends ConsumerWidget {
     return state.map(
       data: (d) => _BalanceCard(
         key: UniqueKey(),
+        padding: padding,
         totalBalance: "${d.value.totalExpenses + d.value.totalIncome}",
         totalIncome: "${d.value.totalIncome}",
         totalExpenses: "${d.value.totalExpenses}",
       ),
       error: (_) => _BalanceCard(
         key: UniqueKey(),
+        padding: padding,
         totalBalance: "Error..",
         totalIncome: "...",
         totalExpenses: "...",
       ),
       loading: (_) => _BalanceCard(
         key: UniqueKey(),
+        padding: padding,
         totalBalance: "Loading..",
         totalIncome: "...",
         totalExpenses: "...",
@@ -36,79 +41,105 @@ class BalanceCard extends ConsumerWidget {
 
 class _BalanceCard extends StatelessWidget {
   const _BalanceCard({
+    super.key,
     required this.totalBalance,
     required this.totalIncome,
     required this.totalExpenses,
-    super.key,
+    required this.padding,
   });
 
   final String totalBalance;
   final String totalIncome;
   final String totalExpenses;
+  final EdgeInsets padding;
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
-    return Builder(
-      builder: (context) {
-        return DefaultTextStyle(
-          style: const TextStyle(color: Colors.white),
-          child: Container(
-            margin: p16,
-            padding: p16,
-            decoration: BoxDecoration(
-              color: theme.primaryColor,
-              // gradient: LinearGradient(
-              //   colors: [
-              //     theme.primaryColor,
-              //   ],
-              // ),
-              borderRadius: const BorderRadius.all(kDefaultRadius),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    final incomeSection = _buildIncomeSection();
+    final expenseSection = _buildExpenseSection();
+    final balance = _buildBalance(theme);
+    final title = _buildTotalBalanceTitle();
+
+    return DefaultTextStyle(
+      style: const TextStyle(color: Colors.white),
+      child: Container(
+        margin: p16,
+        padding: padding,
+        decoration: BoxDecoration(
+          color: theme.primaryColor,
+          borderRadius: const BorderRadius.all(kDefaultRadius),
+        ),
+        child: ResponsiveWidget(
+          builder: (context) {
+            return Column(
               children: [
-                const Text(
-                  "Total Balance",
-                  style: TextStyle(
-                    fontSize: 13,
-                  ),
-                ),
-                Text(
-                  totalBalance,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: theme.textTheme.headlineSmall!.fontSize,
-                  ),
-                ),
+                title,
+                verSpacing8,
+                balance,
+                verSpacing16,
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    _Subsection(
-                      amount: totalIncome,
-                      icon: const Icon(
-                        Icons.arrow_upward_rounded,
-                        color: Colors.green,
-                      ),
-                      title: "Income",
-                    ),
-                    _Subsection(
-                      amount: totalExpenses,
-                      icon: const Icon(
-                        Icons.arrow_downward_rounded,
-                        color: Colors.red,
-                      ),
-                      title: "Expenses",
-                    ),
-                  ],
+                  children: [incomeSection, expenseSection],
                 ),
               ],
-            ),
-          ),
-        );
-      },
+            );
+          },
+          onExtraSmall: (context) {
+            return Center(
+              child: Column(
+                children: [
+                  title,
+                  verSpacing8,
+                  balance,
+                  verSpacing16,
+                  incomeSection,
+                  verSpacing16,
+                  expenseSection
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Text _buildTotalBalanceTitle() {
+    return const Text(
+      "Total Balance",
+      style: TextStyle(
+        fontSize: 13,
+      ),
+    );
+  }
+
+  Text _buildBalance(ThemeData theme) {
+    return Text(
+      totalBalance,
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        color: Colors.white,
+        fontWeight: FontWeight.bold,
+        fontSize: theme.textTheme.headlineSmall!.fontSize,
+      ),
+    );
+  }
+
+  _Subsection _buildIncomeSection() {
+    return _Subsection(
+      amount: totalIncome,
+      icon: const Icon(Icons.arrow_upward_rounded, color: Colors.green),
+      title: "Income    ",
+    );
+  }
+
+  _Subsection _buildExpenseSection() {
+    return _Subsection(
+      amount: totalExpenses,
+      icon: const Icon(Icons.arrow_downward_rounded, color: Colors.red),
+      title: "Expenses",
     );
   }
 }
@@ -128,6 +159,7 @@ class _Subsection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
         DecoratedBox(
           decoration: const BoxDecoration(
