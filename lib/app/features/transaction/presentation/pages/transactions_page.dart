32cd76a22a07b270f533e4267166443371cf7589
sam garg <smlgarg123@gmail.com>
2022-08-30 00:32:32 +0530

    transactions view n page added

---
+++ lib/app/features/transaction/presentation/pages/transactions_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:simple_money_tracker/app/features/transaction/presentation/views/transactions_view.dart';
+
+class TransactionsPage extends StatelessWidget {
+  const TransactionsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: TransactionsView());
+  }
+}
