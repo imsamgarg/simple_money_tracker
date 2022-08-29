743c9af7e2ee1fbd6a973ef860002bc332de19d9
sam garg <smlgarg123@gmail.com>
2022-08-30 00:31:54 +0530

    expenses added to home view

--- lib/app/features/home/presentation/views/home_view.dart
+++ lib/app/features/home/presentation/views/home_view.dart
@@ -1,16 +1,38 @@
+import 'package:custom_utils/custom_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:simple_money_tracker/app/features/summary/presentation/widgets/balance_card.dart';
+import 'package:simple_money_tracker/app/features/transaction/presentation/views/all_expense_transactions.dart';
+import 'package:simple_money_tracker/app/routes/app_routes.dart';
 
 class HomeView extends ConsumerWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container(
+    return Padding16(
       child: Column(
-        children: const [
-          BalanceCard(),
+        children: [
+          const BalanceCard(),
+          verSpacing28,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                "Expenses",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+              ),
+              TextButton(
+                onPressed: () {
+                  GoRouter.of(context).goNamed(AppRoutes.transactions.name);
+                },
+                child: const Text("View All"),
+              ),
+            ],
+          ),
+          verSpacing12,
+          const Expanded(child: AllExpenseTransactions()),
         ],
       ),
     );
