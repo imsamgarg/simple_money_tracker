50ffff262ce8c6cecdfa07e32f0ad79554dca452
sam garg <smlgarg123@gmail.com>
2022-09-01 02:37:39 +0530

    addtransactionfab added as fab

--- lib/app/features/home/presentation/pages/home_page.dart
+++ lib/app/features/home/presentation/pages/home_page.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:simple_money_tracker/app/core/widgets/screen_responsive_widget.dart';
 import 'package:simple_money_tracker/app/features/home/presentation/views/home_view.dart';
-import 'package:simple_money_tracker/app/routes/app_routes.dart';
+import 'package:simple_money_tracker/app/features/home/presentation/widgets/add_transaction_fab.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -11,17 +10,10 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenResponsiveWidget(
       builder: (context, screenType) {
-        return SafeArea(
+        return const SafeArea(
           child: Scaffold(
-            body: const HomeView(),
-            floatingActionButtonLocation:
-                FloatingActionButtonLocation.centerFloat,
-            floatingActionButton: FloatingActionButton(
-              onPressed: () {
-                GoRouter.of(context).goNamed(AppRoutes.addExpense.name);
-              },
-              child: const Icon(Icons.add),
-            ),
+            body: HomeView(),
+            floatingActionButton: AddTransactionFAB(),
           ),
         );
       },
